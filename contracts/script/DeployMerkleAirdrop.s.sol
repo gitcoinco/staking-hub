36ab4fe982e5ccfef4f2cc25f097ecbf8fbd8d1e
0xKurt <kurt@merbeth.io>
2025-03-14 13:35:23 +0100

    updates

--- contracts/script/DeployMerkleAirdrop.s.sol
+++ contracts/script/DeployMerkleAirdrop.s.sol
@@ -7,16 +7,24 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DeployMerkleAirdrop is Script {
     function run() external returns (MerkleAirdrop) {
-        // Load deployment parameters from environment
-        address matchingPool = vm.envAddress("MATCHING_POOL_ADDRESS");
-        address sender = vm.envAddress("SENDER_ADDRESS");
-        address token = vm.envAddress("TOKEN_ADDRESS");
-        bytes32 merkleRoot = vm.envBytes32("MERKLE_ROOT");
-        uint256 chainId = vm.envUint("CHAIN_ID");
-        uint256 poolId = vm.envUint("POOL_ID");
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
-        vm.startBroadcast();
-        MerkleAirdrop airdrop = new MerkleAirdrop(matchingPool, sender, IERC20(token), merkleRoot, chainId, poolId);
+        address matchingPool = 0x5645bF145C3f1E974D0D7FB91bf3c68592ab5012;
+        address sender = 0x5645bF145C3f1E974D0D7FB91bf3c68592ab5012;
+        address token = 0x5e7C95EaF08D6FeD05a8E4BC607Fb682834C74cE;
+        bytes32 merkleRoot = 0x4b76bcc5479b1f30b59a99b588b8bbf09472e15edb8770dad2e1d603b0cf45e9;
+        uint256 chainId = 11155111;
+        uint256 poolId = 706;
+
+        MerkleAirdrop airdrop = new MerkleAirdrop(
+            matchingPool,
+            sender,
+            IERC20(token),
+            merkleRoot,
+            chainId,
+            poolId
+        );
         vm.stopBroadcast();
 
         return airdrop;
