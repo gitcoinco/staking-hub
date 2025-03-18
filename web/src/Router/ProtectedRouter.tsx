72922cce83f6e63006678d2357cf55f8f2de10e7
nijoe1 <nikolaos@gitcoin.co>
2025-03-18 02:11:57 +0200

    chore: improved protected route

--- web/src/Router/ProtectedRouter.tsx
+++ web/src/Router/ProtectedRouter.tsx
@@ -1,29 +1,17 @@
-import React, { PropsWithChildren } from "react";
 import { Outlet } from "react-router";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
 import { LoadingPage } from "@/components/Loading";
+import { NotConnected } from "@/components/NotConnected";
 
-interface RouteProps {
-  fallback: React.FC<PropsWithChildren<any>>;
-  fallbackProps?: any;
-}
-
-export const ProtectedRouter = ({ fallback: Fallback }: RouteProps) => {
+export const ProtectedRouter = () => {
   const { isConnected, isConnecting } = useAccount();
 
   const isLoading = isConnecting;
-
+  if (!isConnected) {
+    return <NotConnected />;
+  }
   if (isLoading) {
     return <LoadingPage />;
-  }
-
-  if (!isConnected) {
-    return (
-      <Fallback>
-        <ConnectButton />
-      </Fallback>
-    );
   }
 
   return <Outlet />;
