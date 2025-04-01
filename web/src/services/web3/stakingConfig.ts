e85267621a0a775c8b56f84108567505a6af4895
nijoe1 <nikolaos@gitcoin.co>
2025-04-01 22:17:46 +0300

    chore: updated final locking contract

--- web/src/services/web3/stakingConfig.ts
+++ web/src/services/web3/stakingConfig.ts
@@ -1,5 +1,7 @@
 import { Address } from "viem";
 
+export const STAKING_CHAIN_ID = 1;
+
 export const getStakingContractsByChainId = (
   chainId: number,
 ):
@@ -11,7 +13,7 @@ export const getStakingContractsByChainId = (
   switch (chainId) {
     case 1:
       return {
-        tokenLock: "0x006384dE9D80C04bF908495676C507EFfF5C7AD2",
+        tokenLock: "0x73d7439cA441da0bED1020a1F1C88bD572f3D57c",
         gtcToken: "0xDe30da39c46104798bB5aA3fe8B9e0e1F348163F",
       };
     case 11155111:
