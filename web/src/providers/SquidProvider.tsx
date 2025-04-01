7c4e85275d7913f77226bdc7f0dd4e33776f2c2e
nijoe1 <nikolaos@gitcoin.co>
2025-04-01 22:16:47 +0300

    chore: updated squid init params with GTC

--- web/src/providers/SquidProvider.tsx
+++ web/src/providers/SquidProvider.tsx
@@ -1,5 +1,4 @@
 import { createContext, useRef, useState } from "react";
-import { NATIVE } from "@allo-team/allo-v2-sdk";
 import { Dialog, Modal } from "@gitcoin/ui";
 import { useClickOutside } from "@gitcoin/ui/hooks/useClickOutside";
 import SquidWidget, { SwapParams } from "@/components/SquidWidget";
@@ -17,10 +16,10 @@ export const SquidContext = createContext<SquidContextType>({
 export const SquidProvider = ({ children }: { children: React.ReactNode }) => {
   const [isSquidOpen, setIsSquidOpen] = useState(false);
   const swapParams: SwapParams = {
-    fromChainId: "1",
+    fromChainId: "42161",
     toChainId: "1",
-    fromTokenAddress: NATIVE,
-    toTokenAddress: NATIVE,
+    fromTokenAddress: "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
+    toTokenAddress: "0xde30da39c46104798bb5aa3fe8b9e0e1f348163f",
   };
   const modalRef = useRef<HTMLDivElement>(null);
   useClickOutside(modalRef, () => {
