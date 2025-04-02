04cff7decff8c04d15771a8064319e6318f4aebf
nijoe1 <nikolaos@gitcoin.co>
2025-04-02 20:30:39 +0300

    chore: added footer into main layout

--- web/src/layouts/MainLayout.tsx
+++ web/src/layouts/MainLayout.tsx
@@ -2,22 +2,22 @@
 
 import { Outlet } from "react-router";
 import { Toaster } from "@gitcoin/ui";
+import Footer from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
 import { Sidebar } from "@/components/Sidebar";
 
 export const MainLayout = () => {
   return (
-    <div style={{ height: "100vh" }} className="flex min-h-full flex-col">
+    <div className="flex min-h-screen flex-col">
       <Toaster />
       <Navbar />
-      <main className="flex flex-1 flex-col px-20 pb-2 pt-10">
-        <div className="flex flex-1">
-          <Sidebar />
-          <div className="ml-8 flex-1">
-            <Outlet />
-          </div>
-        </div>
-      </main>
+      <div className="flex flex-1 px-20 pb-2 pt-10">
+        <Sidebar />
+        <main className="ml-8 flex-1">
+          <Outlet />
+        </main>
+      </div>
+      <Footer />
     </div>
   );
 };
